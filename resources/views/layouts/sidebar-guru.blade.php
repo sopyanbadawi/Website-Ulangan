6727cbf7032f4457fec75304ff72eca5dc1594df
sopyanbadawi <sopyanbadawi@gmail.com>
2026-01-16 15:14:00 +0700

    fix: sidebar guru

--- resources/views/layouts/sidebar-guru.blade.php
+++ resources/views/layouts/sidebar-guru.blade.php
@@ -9,20 +9,20 @@
            -translate-x-full transition-all duration-300 transform
            h-full hidden
            fixed top-0 start-0 bottom-0 z-40
-           bg-white border-e border-gray-200
-           dark:bg-neutral-800 dark:border-neutral-700"
+           bg-white border-e border-gray-200"
     role="dialog" tabindex="-1" aria-label="Sidebar">
     <div class="relative flex flex-col h-full max-h-full ">
         <!-- Header -->
         <header class=" p-4 flex justify-between items-center gap-x-2">
 
-            <a class="flex-none font-semibold text-xl text-black focus:outline-hidden focus:opacity-80 dark:text-white "
-                href="#" aria-label="Brand">Exam</a>
+            <a href="{{route('admin.dashboard')}}" aria-label="Brand" class="flex-none focus:outline-hidden focus:opacity-80 mb-4 ">
+                <img src="{{url('exam.png')}}" alt="Exam" class="h-10 w-auto">
+            </a>
 
             <div class="lg:hidden -me-2">
                 <!-- Close Button -->
                 <button type="button"
-                    class="flex justify-center items-center gap-x-3 size-6 bg-white border border-gray-200 text-sm text-gray-600 hover:bg-gray-100 rounded-full disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:hover:text-neutral-200 dark:focus:text-neutral-200"
+                    class="flex justify-center items-center gap-x-3 size-6 bg-white border border-gray-200 text-sm text-gray-600 hover:bg-gray-100 rounded-full disabled:opacity-50 disabled:pointer-events-none focus:outline-hidden focus:bg-gray-100 "
                     data-hs-overlay="#hs-sidebar-collapsible-group">
                     <svg class="shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                         viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
@@ -39,7 +39,7 @@
 
         <!-- Body -->
         <nav
-            class="h-full overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+            class="h-full overflow-y-auto [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 ">
             <div class="hs-accordion-group pb-0 px-2  w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
                 <ul class="space-y-1">
                     <li>
@@ -54,8 +54,9 @@
                             Dashboard
                         </a>
                     </li>
+                    
                     <li>
-                        <a class=" hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200"
+                        <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm {{ $activeMenu == 'rekap' ? 'bg-blue-50 text-blue-800' : 'text-gray-800 hover:bg-gray-100 bg-white' }} bg-blue-50 text-sm text-blue-800 rounded-lg hover:bg-blue-100 focus:outline-hidden focus:bg-blue-100"
                             href="{{ route('guru.rekap.index') }}">
                             <svg class="size-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="2" stroke="currentColor" class="size-5">
@@ -65,9 +66,10 @@
                             Rekap Akademik
                         </a>
                     </li>
+
                     <li class="hs-accordion" id="account-accordion">
                         <button type="button"
-                            class=" hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-800 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700 dark:text-neutral-200"
+                            class=" hs-accordion-toggle w-full text-start flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-800 rounded-lg hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100"
                             aria-expanded="true" aria-controls="account-accordion-sub-1-collapse-1">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                 stroke-width="2" stroke="currentColor" class="size-4">
@@ -76,14 +78,14 @@
                             </svg>
                             Manajemen Ujian
 
-                            <svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400 "
+                            <svg class="hs-accordion-active:block ms-auto hidden size-4 text-gray-600 group-hover:text-gray-500 "
                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round">
                                 <path d="m18 15-6-6-6 6" />
                             </svg>
 
-                            <svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500 dark:text-neutral-400 "
+                            <svg class="hs-accordion-active:hidden ms-auto block size-4 text-gray-600 group-hover:text-gray-500 "
                                 xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
                                 fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
                                 stroke-linejoin="round">
@@ -97,7 +99,7 @@
                             <ul class="pt-1 ps-7 space-y-1">
                                 <li>
                                     <a class="flex items-center gap-x-3.5 py-2 px-2.5 text-sm {{ $activeMenu == 'ujian' ? 'bg-blue-50 text-blue-800' : 'text-gray-800 hover:bg-gray-100 bg-white' }} bg-blue-50 text-sm text-blue-800 rounded-lg hover:bg-blue-100 focus:outline-hidden focus:bg-blue-100"
-                                        href="{{ route('guru.ujian.index') }}">
+                                        href="{{ route((Auth::user()->role->name == 'superadmin' ? 'admin' : Auth::user()->role->name) . '.ujian.index') }}">
                                         Ujian
                                     </a>
                                 </li>
